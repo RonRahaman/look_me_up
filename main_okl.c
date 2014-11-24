acde4da33cb5d3bb1cdd1a99a377c7f50042d12b
Ron Rahaman <rahaman@cs.uchicago.edu>
2014-11-24 11:04:04 -0600

    commented main_okl.c

--- main_okl.c
+++ main_okl.c
@@ -13,6 +13,11 @@ const long inner_dim = 128;  // Must be a power of 2 for reduction
 
 int main(int argc, char* argv[]) {
 
+  //===========================================================================
+  //   DECLARATIONS
+  //===========================================================================
+ 
+  //---Input parameters--------------------------------------------------------
   // number of lookups
   long n_lookups = (argc < 2) ? 10000000 : atol(argv[1]);  
   // number of gridpoints
@@ -21,27 +26,45 @@ int main(int argc, char* argv[]) {
   double *F_vals;
   // interval for linearly-spaced grid
   double interval = (double) 1 / (F_len - 1);
+  //---------------------------------------------------------------------------
 
-  // Sum of F_vals for lookups
-  double F_sum = 0;
+  //---F_sum-------------------------------------------------------------------
+  // These are the sum of the function values, evaluated in kernel.
+  // They are the cumulative result of the random lookups.
+  
   // Vectors for sums of F(x_i).  Dimensions will be F_sums[0:outer_dim].
   // In kernel, Each outer unit j will reduce is results to F_sum[i].
   // In main, we will need to reduce F_sums to get a single F_sum
   double *F_sums;
 
-  //
-  unsigned long int L_sum = 0;
+  // Sum of all F(x_i) from kernel.  Outside of kernel, F_sums will be reduced
+  // to get F_sum
+  double F_sum = 0;
+  //---------------------------------------------------------------------------
+
+  //---L_sum-------------------------------------------------------------------
+  // These are the number of lookups that are actually done on the device
+  // They are simple counters, implemented as a sanity check.
+
+  // Will be allocated as L_sums[0:outer_dim].  In kernel, each outer unit j
+  // will reduce its results to L_sum[j]
   unsigned long int *L_sums;
+
+  // Cumulative sum of all counts. Outside of kernel, L_sums will be reduced to
+  // L_sum
+  unsigned long int L_sum = 0;
+  //---------------------------------------------------------------------------
 
   // Loop control
   long i;
+  // Timing
+  struct timeval start, end;
+  double wall_time;
 
+  //---OCCA declarations--------------------------------------------------------
   const char *mode = "CUDA";
   int platformID = 0;
   int deviceID   = 0;
-
-  struct timeval start, end;
-  double wall_time;
 
   occaDevice device;
   occaMemory dev_F_sums, dev_F_vals, dev_L_sums;
@@ -51,53 +74,72 @@ int main(int argc, char* argv[]) {
   occaKernelInfo lookupInfo = occaGenKernelInfo();
   occaKernelInfoAddDefine(lookupInfo, "inner_dim", occaLong(inner_dim));
   occaKernelInfoAddDefine(lookupInfo, "outer_dim", occaLong(outer_dim));
+  //---------------------------------------------------------------------------
 
+  //===========================================================================
+  //   BEFORE KERNEL CALL
+  //===========================================================================
+
+  // Print startup message
   printf("Running %0.2e lookups with %0.2e gridpoints in a %0.2f MB array...\n", 
       (double) n_lookups, (double) F_len, (double) F_len*sizeof(double)/1e6);
 
-  device = occaGetDevice(mode, platformID, deviceID);
-
-  lookup = occaBuildKernelFromSource(device, "lookup.okl", "lookup", lookupInfo);
-
-
-  dev_F_vals = occaDeviceMalloc(device, F_len*sizeof(double), NULL);
-  dev_F_sums = occaDeviceMalloc(device, outer_dim*sizeof(double), NULL);
-  dev_L_sums = occaDeviceMalloc(device, outer_dim*sizeof(unsigned long int), NULL);
-
+  // Malloc host memory
   F_vals = (double *) malloc(F_len*sizeof(double));
   F_sums = (double *) calloc( outer_dim, sizeof(double) );
   L_sums = (unsigned long int *) calloc( outer_dim, sizeof(unsigned long int) );
 
-  // Populate values for F(x) on grid
+  // Populate values for F(x) on host
   for (i=0; i<F_len; i++) {
     F_vals[i] = F(i*interval);
   }
 
+  // Setup device
+  device = occaGetDevice(mode, platformID, deviceID);
+
+  // Setup kernel
+  lookup = occaBuildKernelFromSource(device, "lookup.okl", "lookup", lookupInfo);
+
+  // Malloc device memory
+  dev_F_vals = occaDeviceMalloc(device, F_len*sizeof(double), NULL);
+  dev_F_sums = occaDeviceMalloc(device, outer_dim*sizeof(double), NULL);
+  dev_L_sums = occaDeviceMalloc(device, outer_dim*sizeof(unsigned long int), NULL);
+
+  // Host-to-device memcopy
   occaCopyPtrToMem(dev_F_vals, F_vals, F_len*sizeof(double), 0);
   occaCopyPtrToMem(dev_F_sums, F_sums, outer_dim*sizeof(double), 0);
   occaCopyPtrToMem(dev_L_sums, L_sums, outer_dim*sizeof(unsigned long int), 0);
 
+  // Start timing
   occaDeviceFinish(device);
   gettimeofday(&start, NULL);
 
+  //===========================================================================
+  //   THE KERNEL CALL
+  //===========================================================================
+
   occaKernelRun( lookup, dev_F_vals, occaDouble(interval), occaLong(n_lookups), dev_F_sums, dev_L_sums);
 
+  //===========================================================================
+  //   AFTER KERNEL CALL
+  //===========================================================================
+
+  // End timing
   occaDeviceFinish(device);
   gettimeofday(&end, NULL);
-
   wall_time = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec)/1000000.;
 
-  // Copy dev_F_sums to F_sums
+  // Device-to-host memcopy
   occaCopyMemToPtr(F_sums, dev_F_sums, outer_dim*sizeof(double), 0);
   occaCopyMemToPtr(L_sums, dev_L_sums, outer_dim*sizeof(unsigned long int), 0);
 
-  // Get cumulative F_sum
+  // Reduce sums
   for (i=0; i<outer_dim; i++) {
     F_sum += F_sums[i];
     L_sum += L_sums[i];
   }
 
-  // Get timings
+  // Print results
   printf("Lookups in kernel: %lu\n", L_sum);
   printf("Function value: %0.6f\n", F_sum / n_lookups);
   printf("Time:   %0.2e s\n", wall_time);
@@ -111,6 +153,7 @@ int main(int argc, char* argv[]) {
   occaDeviceFree( device );
   free(F_vals);
   free(F_sums);
+  free(L_sums);
 
   return 0;
 }
